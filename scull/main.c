8cbb210feac6bf3e26b314433110c2c09771ea8d
Koustubh Mattikalli <mkoustubh@gmail.com>
2025-12-24 14:31:54 +0530

    scull: device registration

---
+++ scull/main.c
@@ -0,0 +1,48 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/cdev.h>
+
+#include "scull.h"
+
+int scull_major = SCULL_MAJOR;
+int scull_minor = 0;
+int scull_nr_devs = SCULL_NR_DEVS;
+
+module_param(scull_major, int, S_IRUGO);
+module_param(scull_minor, int, S_IRUGO);
+module_param(scull_nr_devs, int, S_IRUGO);
+
+MODULE_AUTHOR("Koustubh Mattikalli");
+MODULE_LICENSE("Dual BSD/GPL");
+
+static int scull_init_module(void)
+{
+	int result;
+	dev_t dev;
+
+	if (scull_major) {
+		dev = MKDEV(scull_major, scull_minor);
+		result = register_chrdev_region(dev, scull_nr_devs, "scull");
+	} else {
+		result = alloc_chrdev_region(&dev, scull_minor, scull_nr_devs, "scull");
+		scull_major = MAJOR(dev);
+	}
+
+	if (result) {
+		printk(KERN_WARNING "scull: can't get major %d\n", scull_major);
+		return result;
+	}
+
+	return 0;
+}
+
+static void scull_exit_module(void)
+{
+	dev_t dev = MKDEV(scull_major, scull_minor);
+
+	unregister_chrdev_region(dev, scull_nr_devs);
+}
+
+module_init(scull_init_module);
+module_exit(scull_exit_module);
---
+++ scull/scull.h
@@ -0,0 +1,12 @@
+#ifndef __SCULL_H__
+#define __SCULL_H__
+
+#ifndef SCULL_MAJOR
+#define SCULL_MAJOR 0	/* Dynamic major by default */
+#endif
+
+#ifndef SCULL_NR_DEVS
+#define SCULL_NR_DEVS 4
+#endif
+
+#endif //__SCULL_H__
